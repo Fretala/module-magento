4e959f05b31d935def65618661fcab6369ed02ed
Rafael Ortega Bueno <programacao@tokdeconforto.com.br>
2014-08-30 03:05:31 -0300

    Amin System Configurations

---
+++ app/code/community/Uecommerce/Fretala/Model/Source/Manualautomatic.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Uecommerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the extension
+ * to newer versions in the future. If you wish to customize the extension
+ * for your needs please refer to http://www.uecommerce.com.br/ for more information
+ *
+ * @category   Uecommerce
+ * @package    Uecommerce_Fretala
+ * @copyright  Copyright (c) 2014 Uecommerce (http://www.uecommerce.com.br/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Fretala module
+ *
+ * @category   Uecommerce
+ * @package    Uecommerce_Fretala
+ * @author     Uecommerce Dev Team
+ */
+class Uecommerce_Fretala_Model_Source_Manualautomatic extends Mage_Core_Model_Config_Data
+{
+	public function toOptionArray(){
+		return array(
+			array('label'=>Mage::helper('fretala')->__('Manual'),'value'=>'manual'),
+			array('label'=>Mage::helper('fretala')->__('Automatic'),'value'=>'automatic')
+		);
+	}
+}
